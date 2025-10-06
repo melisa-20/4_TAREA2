db1d3bfc000eb287807743660e4069f985631ddd
melisa-20 <melisatatianasarmiento@gmail.com>
2025-10-06 09:54:45 -0500

    mi tercera actividad

--- Tarea3.java
+++ Tarea3.java
@@ -1,4 +1,32 @@
 package tarea2;
 
 public class Tarea3 {
+    String nombre;
+    String plato1;
+    String plato2;
+    String plato3;
+    int precio1;
+    int precio2;
+    int precio3;
+
+    public Tarea3() {
+    }
+    public Tarea3(String nombre, String plato1, String plato2, String plato3, int precio1, int precio2, int precio3) {
+        this.nombre = nombre;
+        this.plato1 = plato1;
+        this.plato2 = plato2;
+        this.plato3 = plato3;
+        this.precio1 = precio1;
+        this.precio2 = precio2;
+        this.precio3 = precio3;
+    }
+    void carta(){
+        System.out.println(" ***la carta de hoy*** "+plato1+plato2+plato3);
+    }
+    void cliente(){
+        System.out.println("****"+nombre+"****");
+    }
+    void preciosPlatos(){
+        System.out.println(" ***los precios para el dia de hoy*** "+precio1+" "+precio2+" "+precio3);
+    }
 }
--- Tarea3Main.java
+++ Tarea3Main.java
@@ -2,6 +2,10 @@ package tarea2;
 
 public class Tarea3Main {
     public static void main(String[] args) {
+        Tarea3 tienda=new Tarea3(" tatiana "," **ceviche** "," **pachamanca** "," **tacho** ",12,23,20);
+        tienda.cliente();
+        tienda.carta();
+        tienda.preciosPlatos();
 
     }
 }
